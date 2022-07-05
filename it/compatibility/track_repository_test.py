38fe9571695dbfb56f12f75e0dcb47ae10242572
Mike Baamonde <mike.baamonde@elastic.co>
2022-07-05 14:17:31 -0400

    wip

--- it/compatibility/track_repository_test.py
+++ it/compatibility/track_repository_test.py
@@ -18,35 +18,6 @@
 import pytest
 
 class TestTrackRepository:
-#     track_challenges_to_skip = {
-#         "sql": ["sql"],
-# #        "http_logs": ["append-no-conflicts", "runtime-fields"]
-#     }
-
-    overrides = {
-        "http_logs": {
-            "runtime-fields": {
-                "enable_assertions": False
-            },
-            "append-no-conflicts": {
-                "enable_assertions": False
-            }
-
-        },
-        "sql": {
-            "sql": {
-                "track_params": "ingest_percentage:1,query_percentage:2"
-            }
-        }
-    }
-    def test_track_challenge(self, rally_config, es_cluster, race, track, challenge):
-        kwarg_overrides = self.overrides.get(track, {}).get(challenge, {})
-        ret = race(track, challenge, **kwarg_overrides)
+    def test_track_challenge(self, rally_config, es_cluster, race, track, challenge, race_kwargs):
+        ret = race(track, challenge, **race_kwargs)
         assert ret == 0
-
-# class TestHttpLogs:
-#     def test_http_logs_runtime_fields(self, rally_config, es_cluster, race):
-#         ret = race("http_logs", "runtime-fields", enable_assertions=False)
-
-#     def test_http_logs_append_no_conflicts(self, rally_config, es_cluster, race):
-#         ret = race("http_logs", "append_no_conflicts", enable_assertions=False)
